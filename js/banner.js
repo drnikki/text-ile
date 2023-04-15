d4a8a606246683686be7871f4ee8912001da8ef0
michael <77591445+michael-cxa@users.noreply.github.com>
2023-04-15 02:52:27 -0400

    add twinkle banner code to its own file (not yet imported)

---
+++ js/banner.js
@@ -0,0 +1,62 @@
+const ribbonChoices = "_*':-^";
+
+/**
+ * returns a single row of the twinkle banner based on the current ribbon styles
+ * @param ribbonStyles an array that specifies the order and style and length of each ribbon
+ * @param rowNum current row we are printing, starting from 1
+ * @return one row
+ */
+const printRibbonRow = (ribbonStyles, rowNum = 0) => {
+    let row = '&nbsp;'.repeat(4);
+
+    ribbonStyles.forEach(({style, length}) => {
+        if (rowNum < length) row += style; // add ribbon to row if we're not past its length
+        else if (rowNum === length) row += style === ':' ? 'v' : style; // top ribbon off appropriately
+        else row += '&nbsp;'; // if we're past the ribbon, just add space
+    });
+
+    return row
+};
+
+/**
+ * prints the twinkle banner as specified in the spec doc
+ * @returns {string} twinkle banner
+ */
+const printTwinkleBanner = () => {
+    // decide how many ribbons to use
+    const numRibbons = Math.floor((Math.random() * 11) + 20) // random int from 20-30
+
+    // create top pole
+    let pole = '';
+    pole += '&nbsp;'.repeat(numRibbons + 3) + '___' + '<br>';
+    pole += '='.repeat(numRibbons + 4) + '[]' + '<br>';
+
+    // decide the order of ribbon styles
+    let longestRibbonLength = 0;
+    let ribbonStyles = [];
+    for (let i = 0; i < numRibbons; i++) {
+        const choice = Math.floor(Math.random() * ribbonChoices.length); // pick random char from ribbonChoices string
+        const length =  Math.floor((Math.random() * 9) + 3) // random length between 3 and 11
+        ribbonStyles.push({
+            style: ribbonChoices.charAt(choice),
+            length
+        });
+
+        // update longest ribbon length
+        longestRibbonLength = Math.max(longestRibbonLength, length);
+    }
+
+    // generate first 3 rows of ribbons
+    let ribbons = '';
+    ribbons += printRibbonRow(ribbonStyles) + '[]' + '<br>';
+    ribbons += printRibbonRow(ribbonStyles) + '[' + '<br>';
+    ribbons += printRibbonRow(ribbonStyles) + '|' + '<br>';
+
+    // generate the rest of the ribbons
+    for (let i = 4; i <= longestRibbonLength; i++)
+        ribbons += printRibbonRow(ribbonStyles, i) + '<br>';
+
+    return pole + ribbons;
+};
+
+export default printTwinkleBanner;

4cc152e6c12079a935d71e1441bfb1de371b969e
michael <77591445+michael-cxa@users.noreply.github.com>
2023-04-20 23:17:17 -0400

    add six-column functionality

--- js/chandelier.js
+++ js/chandelier.js
@@ -5,14 +5,13 @@
 
 
 /**
- * These are stub tester files that are separate from any consideration of how the 
- * data might relate to these shapes.  
- * 
+ * These are stub tester files that are separate from any consideration of how the
+ * data might relate to these shapes.
+ *
  * Right now, we just need to get things printing for the prototypes.
  */
 
 let i = 0;
-let textContent = ''; // this is what prints on the receipt.
 
 
 
@@ -44,8 +43,8 @@ function printClouds() {
     // so that it can just be appended to each row
 
     var spacePrefix = numToSpace(startPosition);
- 
-    var wholeCloud = ''; 
+
+    var wholeCloud = '';
     wholeCloud += oneCloud(spacePrefix);
     if (isRain) {
         wholeCloud += makeItRain(cloudCount, spacePrefix);
@@ -55,15 +54,15 @@ function printClouds() {
 }
     // figure out how to render the whole shape, from the top down
     // clouds have 5 rows plus rain if it exists
-   
-    // the receipt printer api needs to run row by row and receive the text as though it 
+
+    // the receipt printer api needs to run row by row and receive the text as though it
     // were going into a <pre> tag.
 
 
 function oneCloud(prefix) {
     var row = '';
-    var oneWholeCloud = ''; 
-    
+    var oneWholeCloud = '';
+
     row = prefix + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(()<br/>";
     oneWholeCloud = row;
     row = prefix +"&nbsp;&nbsp;&nbsp;&nbsp;(((())<br/>";
@@ -85,7 +84,7 @@ function makeItRain(cloudCount, prefix) {
 
     // we'd do something about the width of the rain based on the Clouds
     let someRain = ''; // the contents of the rain.
-    
+
     // FOR ONE CLOUD
     for (let r = 0; r < rainRows; r++) {
         someRain += prefix + randomSpacer(1, 7) + "; <br/>"; // @TODO randomize the rain.
@@ -95,7 +94,7 @@ function makeItRain(cloudCount, prefix) {
 }
 
 
-// This helper function just converts a number to nonbreaking spaces.  
+// This helper function just converts a number to nonbreaking spaces.
 // That's all it does
 function numToSpace(howMany) {
     // convert # to spaces
@@ -111,7 +110,7 @@ function numToSpace(howMany) {
 // and returns that # in spaces
 function randomSpacer(min, max) {
    let theNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-   
+
    return numToSpace(theNumber);
 }
 
@@ -121,7 +120,7 @@ function randomSpacer(min, max) {
 
 
 function printChandelierA() {
-    /// 
+    ///
     /// SET VARIABLES
     ///
     // Math.floor(Math.random() * (max - min + 1)) + min
@@ -135,7 +134,7 @@ function printChandelierA() {
     // when 6, 13
     if (brassRows == 4) {
         crystalWidth = 5;
-        crystalPrefix = 6; 
+        crystalPrefix = 6;
     } else if (brassRows == 5) {
         crystalWidth = 9;
         crystalPrefix = 4;
@@ -145,7 +144,7 @@ function printChandelierA() {
     }
 
     // print the brass to start?
-    // these are always the same 
+    // these are always the same
     var row = numToSpace(7) + "<^><br/>";
     row += numToSpace(7) + "^^^<br/>";
     row += numToSpace(6) + "<^^^><br/>";
@@ -160,7 +159,7 @@ function printChandelierA() {
     // time for Crystals. Width of crystals determined by
     for(let c =0;c < crystalRows; c++) {
         row += numToSpace(crystalPrefix);
-        // 
+        //
         for (let x = 0; x < crystalWidth; x++) {
             row += "|";
         }
@@ -297,20 +296,16 @@ const reverseBanner = (banner) => {
 };
 
 
-
-// put stuff onto the receipt
-
-for (let y = 0; y < 5; y++) {
-    textContent += printClouds();
-    textContent += "<br/><br/>";
-    textContent += printChandelierA();
-    textContent += printChandelierB() + "<br/>";
-    textContent += printTwinkleBanner() + "<br/>";
-
+let ReceiptPlaces = document.querySelectorAll('.receipt'); // list of all receipts (only 1 for index.html)
+for (let i = 0; i < ReceiptPlaces.length; i++) {
+    let textContent = ''; // this is what prints on the receipt.
+    for (let y = 0; y < 5; y++) {
+        textContent += printClouds();
+        textContent += "<br/><br/>";
+        textContent += printChandelierA();
+        textContent += printChandelierB() + "<br/>";
+        textContent += printTwinkleBanner() + "<br/>";
+    }
+    // FINALLY: everything that we did - put it onto the receipt
+    ReceiptPlaces[i].innerHTML = textContent;
 }
- 
-
-
-// FINALLY: everything that we did - put it onto the receipt
-let ReceiptPlace = document.querySelector('#receipt');
-ReceiptPlace.innerHTML = textContent;

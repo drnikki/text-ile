38b58c5e947136d45bed8ef3d96c1f682870e805
Leonardo Bueno <leonardobueno@Leonardos-MacBook-Pro.local>
2023-04-25 22:30:03 -0400

    print herringbone works

--- js/chandelier.js
+++ js/chandelier.js
@@ -785,9 +785,36 @@ const printRope = () => {
     return printRopePiece(false) + printRopePiece(true) + printRopePiece(false);
 }
 
-const printHerringBone = () => {
-    
+/**
+ * print herringbone
+ */
+const printHerringBone = (rows=9) => {
+    const columns = 40;
+    const pattern = '}}}--';
+    let ret = '';
+    let index = 0;
+
+    // iterate through number of rows and columns
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+            // take current index of pattern string mod pattern string length and to return string
+            ret += pattern[index % pattern.length];
+            index++;
+        }
+        ret += '<br/>';
+
+        // if before halfway point, decrease index by one (creates symmetry)
+        if (i < Math.floor(rows / 2)) {
+            index--;
+        } 
+        // else, increase index by one
+        else {
+            index++;
+        }
+    }
+    return ret;
 }
+
 
 // FINALLY: everything that we did - put it onto the receipt
 
@@ -802,6 +829,7 @@ for (let i = 0; i < ReceiptPlaces.length; i++) {
         textContent += printTwinkleBanner() + "<br/>";
         textContent += printStarburst() + "<br/>";
         textContent += printRope() + "<br/>";
+        textContent += printHerringBone() + "<br/>";
     }
     // FINALLY: everything that we did - put it onto the receipt
     ReceiptPlaces[i].innerHTML = textContent;

fca3628a1ba404a46e79dd0baac44a686c225a64
Xiaoyu-cs <xiaoyu.wei.gr@dartmouth.edu>
2023-05-05 03:44:57 -0400

    visualization

--- js/receipt.js
+++ js/receipt.js
@@ -1,15 +1,89 @@
-import {printTriangle1, printTriangle2} from "./sprite/triangles.js";
+import { printTriangle1, printTriangle2 } from "./sprite/triangles.js";
 import printBug from "./sprite/bug.js";
 import printClouds from "./sprite/clouds.js";
-import {printChandelierA, printChandelierB} from "./sprite/chandelier.js";
+import { printChandelierA, printChandelierB } from "./sprite/chandelier.js";
 import printTwinkleBanner from "./sprite/banner.js";
 import printStarburst from "./sprite/starburst.js";
 import printRope from "./sprite/rope.js";
-import {printBasketWeave, printHerringBone, printDiamond, printSeedStitch} from "./sprite/pattern.js";
+import { printBasketWeave, printHerringBone, printDiamond, printSeedStitch } from "./sprite/pattern.js";
 import printMarioCoinBox from "./sprite/coinBox.js";
 
+let list1 =
+    `
+    <div>
+        <pre>${printTwinkleBanner()}</pre><br>
+        <pre>${printTwinkleBanner()}</pre><br>
+        <pre>${printStarburst()}</pre><br>
+        <pre>${printTwinkleBanner()}</pre><br>
+        <pre>${printStarburst()}</pre><br>
+    </div>
+    `
 
+let list2 =
+    `
+    <div>
+        <div class='fn-clear'><pre>${printChandelierA()}</pre></div>
+        <br>
+        <div class='fn-clear'>
+            <div class='fl' style="width:50%">
+            <pre>
+            ${printChandelierB()}<br>
+            ${printChandelierA()}<br>
+            ${printChandelierB()}<br>
+            ${printChandelierA()}<br>
+                        ${printChandelierB()}<br>
+            ${printChandelierA()}<br>
+                        ${printChandelierB()}<br>
+            ${printChandelierA()}<br>
+            </pre>
+            </div>
+            <div class='fr' style="width:50%"><pre>
+            ${printChandelierA()}<br>
+            ${printChandelierB()}<br>
+            ${printChandelierA()}<br>
+            ${printChandelierB()}<br>
+            </pre ></div >
+        </div >
+    </div >
+    `
 
+let list3 =
+    `<div>
+<div class='fn-clear'>
+<div class='fl'><pre>${printClouds()}</pre></div>
+<div class='fl'><pre>${printClouds()}</pre></div>
+</div>
+<pre>${printMarioCoinBox()}</pre>
+<div class='fn-clear'>
+<div class='fl'><pre>${printClouds()}</pre></div>
+<div class='fl'><pre>${printClouds()}</pre></div>
+<div class='fl'><pre>${printClouds()}</pre></div>
+</div>
+<div class='fn-clear'>
+<div class='fl'><pre>${printClouds()}</pre></div>
+<div class='fl'><pre>${printClouds()}</pre></div>
+</div>
+<div class='fn-clear'>
+<div class='fr'><pre>${printClouds()}</pre></div>
+<div class='fr'><pre>${printClouds()}</pre></div>
+</div>
+</div>
+    `
+let list4 = `
+<div><pre  style='margin:auto;display:block;width:300px'>${printTriangle1()}</pre></div>
+<div><pre  style='margin:auto;display:block;width:300px'>${printTriangle2()}</pre></div>
+<div><pre  style='margin:auto;display:block;width:300px'>${printBug()}</pre></div>
+<div><pre  style='margin:auto;display:block;width:300px'>${printTriangle1()}</pre></div>
+<div><pre  style='margin:auto;display:block;width:300px'>${printTriangle2()}</pre></div>
+`
+
+document.querySelector('.receipt1').innerHTML = list1;
+document.querySelector('.receipt2').innerHTML = list2;
+document.querySelector('.receipt3').innerHTML = list3;
+document.querySelector('.receipt4').innerHTML = list4;
+document.querySelector('.receipt5').innerHTML = list3;
+document.querySelector('.receipt6').innerHTML = list2;
+document.querySelector('.receipt7').innerHTML = `<pre>${printRope()}</pre>`;
 let ReceiptPlaces = document.querySelectorAll('.receipt'); // list of all receipts (only 1 for index.html)
 for (let i = 0; i < ReceiptPlaces.length; i++) {
     let textContent = ''; // this is what prints on the receipt.
@@ -31,5 +105,5 @@ for (let i = 0; i < ReceiptPlaces.length; i++) {
         textContent += printMarioCoinBox() + "<br/>";
     }
     // FINALLY: everything that we did - put it onto the receipt
-    ReceiptPlaces[i].innerHTML = textContent;
+    // ReceiptPlaces[i].innerHTML = textContent;
 }
